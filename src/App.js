379ab2cc53016258a93b22a964fa2682ac9aaaea
Mehadi Hasan <themehadihasan@gmail.com>
2022-10-20 18:31:55 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import Search from './Components/Search/Search';
-import BackgroundImage from './background.png';
 
 function App() {
 
   return (
-    <div className="app container-fluid pt-5" style={{backgroundImage: `url(${BackgroundImage})`}}>
+    <div className="app container-fluid pt-5">
       <Search />
     </div>
   );
